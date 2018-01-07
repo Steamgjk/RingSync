e35a1a389370a89895ca5573a46880ba78362aef
Your Name <you@example.com>
2018-01-07 11:09:57 +0800

    add some structs

--- t_ring/tensorflow/rdma_t.h
+++ t_ring/tensorflow/rdma_t.h
@@ -3,9 +3,13 @@
 #include <vector>
 #include <string>
 #include <pthread.h>
+#include <stdarg.h>
 #if HAVE_RDMA
 #include <rdma/rdma_cma.h>
 
+#define MAX_CONCURRENCY 20
+
+void log_info(const char *format, ...);
 
 typedef struct _data_list_
 {
@@ -55,6 +59,78 @@ struct context
 	uint32_t peer_rkey;
 	bool remote_idle;
 };
+
+typedef struct _key_exchange_
+{
+	int id;
+	uint64_t md5;
+	struct
+	{
+		uint64_t addr;
+		uint32_t rkey;
+	} key_info[MAX_CONCURRENCY];
+
+} _key_exch;
+
+typedef struct _e_
+{
+//register buffer for remote to write
+	char *           buffer[MAX_CONCURRENCY];
+	struct ibv_mr *  buffer_mr[MAX_CONCURRENCY];
+
+//register ack mem is used for write to remote
+	_ack_*			 ack[MAX_CONCURRENCY];
+	struct ibv_mr *  ack_mr[MAX_CONCURRENCY];
+
+	uint64_t peer_addr[MAX_CONCURRENCY];
+	uint32_t peer_rkey[MAX_CONCURRENCY];
+
+//indicate current status of each peer exchange
+	int 			 is_busy[MAX_CONCURRENCY];
+// index 0: store for local as tx    (to send)
+// index 1: used to recv the remote info (to recv)
+	_key_exch*       k_exch[2];
+	struct ibv_mr*   k_exch_mr[2];
+} extend_info;
+
+
+//for server to use
+struct conn_context
+{
+	char *buffer;
+	struct ibv_mr *buffer_mr;
+
+	struct message *msg;
+	struct ibv_mr *msg_mr;
+
+	FILE* fd;
+	char file_name[MAX_FILE_NAME];
+
+
+	extend_info s_new_ctx;
+};
+
+//for client to use
+struct client_context
+{
+	char *buffer;
+	struct ibv_mr *buffer_mr;
+
+	struct message *msg;
+	struct ibv_mr *msg_mr;
+
+	uint64_t peer_addr;
+	uint32_t peer_rkey;
+
+	FILE* fd;
+	const char *file_name;
+
+	// for client used
+	extend_info c_new_ctx;
+
+};
+
+
 
 
 void test_rdma_header();
