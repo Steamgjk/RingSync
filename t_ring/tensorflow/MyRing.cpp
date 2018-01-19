9339d73fab042db23d2a4f3b250819490de270e8
Your Name <you@example.com>
2018-01-19 15:48:26 +0800

    aa

--- t_ring/tensorflow/MyRing.cpp
+++ t_ring/tensorflow/MyRing.cpp
@@ -1378,8 +1378,9 @@ node_item* MyRing::send_tensor_single(struct rdma_cm_id *id, node_item* head_ptr
 {
 	struct context *ctx = (struct context *)id->context;
 	while (head_ptr->next == nullptr)
-		//std::this_thread::sleep_for(std::chrono::nanoseconds(100000));
-		sleep(1);
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	//std::this_thread::sleep_for(std::chrono::nanoseconds(100000));
+	//sleep(1);
 	if (shut_down)
 	{
 		return NULL;
