3e9e2e8a0fbb6012e9e4345c69b60101668e4be6
Your Name <you@example.com>
2018-01-21 17:08:24 +0800

    vgg-40ms

--- t_ring/tensorflow/MyRing.cpp
+++ t_ring/tensorflow/MyRing.cpp
@@ -1377,7 +1377,7 @@ node_item* MyRing::send_tensor_single(struct rdma_cm_id *id, node_item* head_ptr
 {
 	struct context *ctx = (struct context *)id->context;
 	while (head_ptr->next == nullptr)
-		std::this_thread::sleep_for(std::chrono::milliseconds(30));
+		std::this_thread::sleep_for(std::chrono::milliseconds(40));
 	//std::this_thread::sleep_for(std::chrono::nanoseconds(500000)); // Mnist
 	//std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	if (shut_down)
