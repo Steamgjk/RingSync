a4b082c34f80aa454d43d2ef0578ec563edf231d
Your Name <you@example.com>
2018-03-08 15:39:43 +0800

    aa

--- demo_folder/common.c
+++ demo_folder/common.c
@@ -17,60 +17,6 @@ static struct context *s_ctx = NULL;
 
 //here
 
-void build_connection(struct rdma_cm_id *id)
-{
-  struct ibv_qp_init_attr qp_attr;
-
-  build_context(id->verbs);
-  build_qp_attr(&qp_attr);
-
-  TEST_NZ(rdma_create_qp(id, s_ctx->pd, &qp_attr));
-}
-
-void build_context(struct ibv_context *verbs)
-{
-  if (s_ctx)
-  {
-    if (s_ctx->ctx != verbs)
-      rc_die("cannot handle events in more than one context.");
-
-    return;
-  }
-
-  s_ctx = (struct context *)malloc(sizeof(struct context));
-
-  s_ctx->ctx = verbs;
-
-  TEST_Z(s_ctx->pd = ibv_alloc_pd(s_ctx->ctx));
-  TEST_Z(s_ctx->comp_channel = ibv_create_comp_channel(s_ctx->ctx));
-  TEST_Z(s_ctx->cq = ibv_create_cq(s_ctx->ctx, 10, NULL, s_ctx->comp_channel, 0)); /* cqe=10 is arbitrary */
-  TEST_NZ(ibv_req_notify_cq(s_ctx->cq, 0));
-
-  TEST_NZ(pthread_create(&s_ctx->cq_poller_thread, NULL, poll_cq, NULL));
-}
-
-void build_params(struct rdma_conn_param *params)
-{
-  memset(params, 0, sizeof(*params));
-
-  params->initiator_depth = params->responder_resources = 1;
-  params->rnr_retry_count = 7; /* infinite retry */
-}
-
-void build_qp_attr(struct ibv_qp_init_attr *qp_attr)
-{
-  memset(qp_attr, 0, sizeof(*qp_attr));
-
-  qp_attr->send_cq = s_ctx->cq;
-  qp_attr->recv_cq = s_ctx->cq;
-  qp_attr->qp_type = IBV_QPT_RC;
-
-  qp_attr->cap.max_send_wr = 10;
-  qp_attr->cap.max_recv_wr = 10;
-  qp_attr->cap.max_send_sge = 1;
-  qp_attr->cap.max_recv_sge = 1;
-}
-
 
 
 void rc_disconnect(struct rdma_cm_id *id)
@@ -83,6 +29,8 @@ void rc_die(const char *reason)
   fprintf(stderr, "%s\n", reason);
   exit(EXIT_FAILURE);
 }
+
+
 
 struct ibv_pd * rc_get_pd()
 {
