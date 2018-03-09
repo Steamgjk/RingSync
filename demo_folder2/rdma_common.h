e4b32fdbc0ee5a1ddd47f3e816c7536851854f0f
Your Name <you@example.com>
2018-03-09 12:44:03 +0800

    rdma_read_write

--- demo_folder2/rdma_common.h
+++ demo_folder2/rdma_common.h
@@ -31,7 +31,7 @@
 	fprintf(stderr, "%s : %d : ERROR : "msg, __FILE__, __LINE__, ## args);\
 }while(0);
 
-// #define ACN_RDMA_DEBUG
+#define ACN_RDMA_DEBUG
 
 #ifdef ACN_RDMA_DEBUG
 /* Debug Macro */
@@ -60,15 +60,17 @@
  *
  * For details see: http://gcc.gnu.org/onlinedocs/gcc/Type-Attributes.html
  */
-struct __attribute((packed)) rdma_buffer_attr {
-  uint64_t address;
-  uint32_t length;
-  union stag {
-	  /* if we send, we call it local stags */
-	  uint32_t local_stag;
-	  /* if we receive, we call it remote stag */
-	  uint32_t remote_stag;
-  }stag;
+struct __attribute((packed)) rdma_buffer_attr
+{
+	uint64_t address;
+	uint32_t length;
+	union stag
+	{
+		/* if we send, we call it local stags */
+		uint32_t local_stag;
+		/* if we receive, we call it remote stag */
+		uint32_t remote_stag;
+	} stag;
 };
 /* resolves a given destination name to sin_addr */
 int get_addr(char *dst, struct sockaddr *addr);
@@ -83,8 +85,8 @@ void show_rdma_buffer_attr(struct rdma_buffer_attr *attr);
  * @cm_event: where the event will be stored
  */
 int process_rdma_cm_event(struct rdma_event_channel *echannel,
-		enum rdma_cm_event_type expected_event,
-		struct rdma_cm_event **cm_event);
+                          enum rdma_cm_event_type expected_event,
+                          struct rdma_cm_event **cm_event);
 
 /* Allocates an RDMA buffer of size 'length' with permission permission. This
  * function will also register the memory and returns a memory region (MR)
@@ -94,8 +96,8 @@ int process_rdma_cm_event(struct rdma_event_channel *echannel,
  * @permission: OR of IBV_ACCESS_* permissions as defined for the enum ibv_access_flags
  */
 struct ibv_mr* rdma_buffer_alloc(struct ibv_pd *pd,
-		uint32_t length,
-		enum ibv_access_flags permission);
+                                 uint32_t length,
+                                 enum ibv_access_flags permission);
 
 /* Frees a previously allocated RDMA buffer. The buffer must be allocated by
  * calling rdma_buffer_alloc();
@@ -111,9 +113,9 @@ void rdma_buffer_free(struct ibv_mr *mr);
  * @permission: OR of IBV_ACCESS_* permissions as defined for the enum ibv_access_flags
  */
 struct ibv_mr *rdma_buffer_register(struct ibv_pd *pd,
-		void *addr,
-		uint32_t length,
-		enum ibv_access_flags permission);
+                                    void *addr,
+                                    uint32_t length,
+                                    enum ibv_access_flags permission);
 /* Deregisters a previously register memory
  * @mr: Memory region to deregister
  */
@@ -126,8 +128,8 @@ void rdma_buffer_deregister(struct ibv_mr *mr);
  *          atleast this size.
  */
 int process_work_completion_events(struct ibv_comp_channel *comp_channel,
-		struct ibv_wc *wc,
-		int max_wc);
+                                   struct ibv_wc *wc,
+                                   int max_wc);
 
 /* prints some details from the cm id */
 void show_rdma_cmid(struct rdma_cm_id *id);
